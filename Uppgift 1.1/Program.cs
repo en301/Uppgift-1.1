e117c200b3d8cc461f1fc8dc5726d9ea5bf4b171
emsj06001 <emsj06001@E-PW024NWP>
2023-09-06 10:28:11 +0200

    Add project files.

---
+++ Uppgift 1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; //Det är namnet på biblioteket vi använder oss av
+
+namespace Uppgift_1_1 //Namnet på gruppen kod under
+{
+    class program //Är inte säker på vad den gör men tror den samlar kod
+    {
+        static void Main(string[] args) //Ingen aning
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); //Skriver ut texten mellan "" dessa tecken
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\."); // samma som förra fast jag använder \ för att kunna skriva ut tecken
+            Console.ReadKey(); // tar bort all onödig text efte
+        }
+    }    
+}
